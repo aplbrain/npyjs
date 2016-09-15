ed26466b5248179a97b3c790b620b88b6bf38300
Jordan Matelsky <j6k4m8@gmail.com>
2016-09-15 17:25:58 -0400

    Working nd <255 values

--- npy.js
+++ npy.js
@@ -1,5 +1,3 @@
-PREFIX = "data:application/octet-stream;base64,";
-
 (function(G) {
 
 
@@ -8,7 +6,7 @@ PREFIX = "data:application/octet-stream;base64,";
             // Do nothing.
         }
 
-        load(filename) {
+        load(filename, callback) {
             /*
             Loads an array from a stream of bytes.
             */
@@ -19,19 +17,36 @@ PREFIX = "data:application/octet-stream;base64,";
                         var reader = new FileReader();
                         reader.addEventListener("loadend", function() {
                             var res = reader.result;
+                            var headerLength = res.indexOf('}') + 1;
+                            var header = JSON.parse(
+                                res.slice(10, headerLength)
+                                .replace(/\'/g, '"')
+                                .replace("False", "false")
+                                .replace("(", "[")
+                                .replace(/,*\),*/g, "]")
+                            );
+                            var shape = header.shape;
+                            G.header = header;
+
                             var array = (
-                                (res.slice(res.indexOf('}') + 1))
-                                // .slice(21 - 9)
+                                (res.slice(headerLength))
                                 .split('')
                             ).map(i => i.charCodeAt(0));
-                            console.log(res);
+
+                            while (array[0] === 32) {
+                                array = array.slice(1);
+                            }
+                            array = array.slice(1);
 
                             G.array = array;
                             var nums = [];
-                            // for (var i = 0; i < array.length; i += 8) {
-                            //     nums.push(array[i]);
-                            // }
+                            for (var i = 0; i < array.length; i += 8) {
+                                nums.push(array[i]);
+                            }
                             G.nums = nums;
+
+                            // Perform reshape.
+                            callback(nums, shape);
                         });
                         reader.readAsBinaryString(content);
                     });
@@ -43,5 +58,38 @@ PREFIX = "data:application/octet-stream;base64,";
     G.npyjs = npyjs;
 })(this);
 
+
+
+
+tests = [
+    [],
+    [91,48,83,47,36],
+    [1,71,70,59,77,32,63,67,40,75],
+    [74,51,26,66,73,96,21,26,35,88,52,49,28,63,11]
+];
+
+function arraysAreEqual(a, b) {
+    return a === b || (
+        !!a && !!b &&
+        a.length === b.length &&
+        a.map((val, idx) => val === b[idx])
+            .reduce((prev, cur) => (prev && cur), true)
+    );
+}
+
 n = new npyjs();
-n.load('blah.npy')
+
+/*
+// Sanity tests:
+n.load('1d-0.npy', (a, n) => { console.log(arraysAreEqual(a, tests[0])) })
+n.load('1d-5.npy', (a, n) => { console.log(arraysAreEqual(a, tests[1])) })
+n.load('1d-10.npy', (a, n) => { console.log(arraysAreEqual(a, tests[2])) })
+n.load('1d-15.npy', (a, n) => { console.log(arraysAreEqual(a, tests[3])) })
+
+// Throughput tests:
+// n.load('2d-100x100.npy', (a, n) => { console.log(arraysAreEqual(a.slice(0, 3), [105, 169, 119])) })
+// n.load('2d-1000x1000.npy', (a, n) => { console.log(arraysAreEqual(a.slice(0, 3), [175, 118, 110])) })
+
+// Dimensionality tests:
+n.load('3d-2x2x2.npy', (a, n) => { console.log(arraysAreEqual(a, [14, 231, 252, 169, 44, 140, 109, 42])) })
+*/
