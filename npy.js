7eae4455b8b3676f59d9cacde7eac4da9a6feebb
Jordan Matelsky <j6k4m8@gmail.com>
2017-07-31 10:12:56 -0400

    Add test suites

--- npy.js
+++ npy.js
@@ -1,20 +1,4 @@
-/*
-Copyright 2016 The Johns Hopkins University Applied Physics Laboratory
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-(function(G) {
+npyjs = (function(G) {
 
     class npyjs {
         constructor(opts) {
@@ -45,6 +29,41 @@ limitations under the License.
             return result;
         }
 
+        parse(res) {
+            let self = this;
+            var headerLength = res.indexOf("}") + 1;
+            var header = JSON.parse(
+                res.slice(10, headerLength)
+                .replace(/\'/g, '"')
+                .replace("False", "false")
+                .replace("(", "[")
+                .replace(/,*\),*/g, "]")
+            );
+            var shape = header.shape;
+            G.header = header;
+
+            var array = (
+                (res.slice(headerLength))
+                .split("")
+            ).map(i => i.charCodeAt(0));
+
+            while (array[0] === 32) {
+                array = array.slice(1);
+            }
+            array = array.slice(1);
+
+            // G.array = array;
+            var nums = [];
+            for (var i = 8; i < array.length + 8; i += 8) {
+                nums.push(self._parseBytes(array.slice(i - 8, i)));
+            }
+            // G.nums = nums;
+            return {
+                nums,
+                shape
+            }
+        }
+
         load(filename, callback) {
             /*
             Loads an array from a stream of bytes.
@@ -56,37 +75,12 @@ limitations under the License.
                         var content = i;
                         var reader = new FileReader();
                         reader.addEventListener("loadend", function() {
-                            var res = reader.result;
-                            var headerLength = res.indexOf("}") + 1;
-                            var header = JSON.parse(
-                                res.slice(10, headerLength)
-                                .replace(/\'/g, '"')
-                                .replace("False", "false")
-                                .replace("(", "[")
-                                .replace(/,*\),*/g, "]")
-                            );
-                            var shape = header.shape;
-                            G.header = header;
+                            var text = reader.result;
 
-                            var array = (
-                                (res.slice(headerLength))
-                                .split("")
-                            ).map(i => i.charCodeAt(0));
-
-                            while (array[0] === 32) {
-                                array = array.slice(1);
-                            }
-                            array = array.slice(1);
-
-                            G.array = array;
-                            var nums = [];
-                            for (var i = 8; i < array.length + 8; i += 8) {
-                                nums.push(self._parseBytes(array.slice(i - 8, i)));
-                            }
-                            G.nums = nums;
+                            var res = self.parse(text);
 
                             // Perform reshape.
-                            callback(nums, shape);
+                            callback(res.nums, res.shape);
                         });
                         reader.readAsBinaryString(content);
                     });
@@ -96,4 +90,6 @@ limitations under the License.
     }
 
     G.npyjs = npyjs;
+
+    return npyjs;
 })(this);
