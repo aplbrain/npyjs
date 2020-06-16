83b066f9e82922d52a0b1af7e8d2362aa948ed2f
Jordan Matelsky <j6k4m8@gmail.com>
2020-06-15 22:50:15 -0400

    Update test-cases

--- test/generate-test-data.py
+++ test/generate-test-data.py
@@ -4,9 +4,11 @@ import json
 records = {}
 
 for dimensions in [(10,), (65, 65), (100, 100, 100), (4, 4, 4, 4, 4)]:
-    for dtype in ["int8", "int64", "float32", "float64"]:
+    for dtype in ["int8", "int16", "int64", "float32", "float64"]:
         name = f"./data/{'x'.join(str(i) for i in dimensions)}-{dtype}"
         data = np.random.randint(0, 255, dimensions).astype(dtype)
         records[name] = data.ravel()[-5:].tolist()
-        np.save(name, data) 
-json.dump(records, open("records.json", "w"), )
+        np.save(name, data)
+json.dump(
+    records, open("records.json", "w"),
+)
